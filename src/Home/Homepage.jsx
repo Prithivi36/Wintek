7de5c1f46a72a60bacb5a38994c129c02a01d459
Prithivi36 <prithivipappian@gmail.com>
2024-05-11 13:13:22 +0530

    Home Page Created

---
+++ src/Home/Homepage.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Software from '../assets/Softwaredev.jpg'
+
+function Homepage() {
+  return (
+    <>
+    <div  className="row mb-2 align-items-center justify-content-around">
+        <div className="image   p-5 col-md">
+            <div className="">
+                <img className=' img-fluid' src={Software} alt="" />
+            </div>
+        </div>
+        <div className="Textual px-md-0 px-4 col-md">
+            <div className="">
+                <h1 className='h-text-indigo'>WINTEK</h1>
+                <h4>A complete software development</h4>
+                <p className='me-lg-3'>
+                Wintek is complete software development service headquartered at Bangalore.
+                 We are expertise in providing a comprehensive and tailor-made solutions to meet customers delight.
+                </p>
+                <button className='btn btn-dark rounded-5'>Learn More</button>
+            </div>
+            <div className="mt-5 mb-5 mb-md-0">
+                <a className='me-4' href="https://www.google.com/maps/@12.955226,77.542659,18z?hl=en&entry=ttu">
+                    <i className="bi p-2 text-light rounded-5 bg-dark bi-geo-alt-fill"></i>
+                </a>
+                <a className='me-4' href="mailto:info@wintekindia.com">
+                    <i className="bi p-2 text-light rounded-5 bg-dark  bi-envelope-fill"></i>
+                </a>
+                <a className='me-4' href="tel:+918695353002">
+                    <i className="bi p-2 text-light rounded-5 bg-dark bi-telephone-fill"></i>
+                </a>
+            </div>
+        </div>
+        
+    </div>
+    </>
+  )
+}
+
+export default Homepage
