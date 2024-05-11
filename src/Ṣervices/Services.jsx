e5fd7ba436185e228d5eb5da9b0708bfa25ce6b7
Prithivi36 <prithivipappian@gmail.com>
2024-05-11 15:33:34 +0530

    Implemented carousel fro services

--- src/Ṣervices/Services.jsx
+++ src/Ṣervices/Services.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Application from '../assets/Application.svg'
+import data from './servdet.js'
 
 function Services() {
   return (
@@ -7,22 +7,55 @@ function Services() {
     <div  id='services' className="bg-dark py-5 mt-4 text-light">
         <h1 className='m-4 p-3  highlight'>Our Services</h1>
         <div className="px-md-5">
-            <div className="bg-indigo rounded-5 row mb-5 mx-3 mx-md-5">
+
+        <div  id="myCar" className="carousel bg-dark text-light slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+            
+
+            {data.map((d,i)=>{
+                return(
+                    <div key={i} className={`carousel-item ${i===0?"active":'no'}`}>
+                    <div className="bg-indigo host rounded-5 row px-lg-5 mb-5 mx-3 mx-md-5">
                 <div className="container py-4 col-md">
-                    <img className='img-fluid' src={Application} alt="" />
+                    <img className='img-fluid' src={d.image} alt="" />
                 </div>
                 <div className='d-flex flex-column justify-content-center align-items-center col-md'>
-                    <h1 className='py-4'>Software Application development</h1>
+                    <h1 className='py-4'>{d.heading}</h1>
                     <div className="thing">
-                    <p>Application software (applications or apps) to help users
-                         perform tasks. Office productivity suites, data management software, media players and security.</p>
-                         <p>
-                         Building dynamic and interactive web-based applications that can run on various devices and browsers. This could
-                          include e-commerce platforms, content management systems (CMS), customer relationship management (CRM) systems, and more.
-                         </p>
+                    <p>
+                        {d.description}
+                    </p>
                     </div>
                 </div>
+                <div className="d-md-flex align-items-center justify-content-center col col-md-1">
+                    <p className='text-center'>
+                    <i class="bi bi-arrow-right"></i>
+                    </p>
+                </div>
+                </div>
             </div>
+                )
+            })}
+        </div>
+        <a className="carousel-control-prev" href="#myCar" role="button" data-bs-slide="prev">
+  </a>
+  <a className="carousel-control-next" href="#myCar" role="button" data-bs-slide="next">
+  </a>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
         </div>
     </div>
     </>
---
+++ src/Ṣervices/servdet.js
@@ -0,0 +1,24 @@
+import applicationimage from '../assets/Application.png'
+
+const data=[
+
+    {"heading":"Software Development",
+    "image":applicationimage,
+        "description":"Application software (applications or apps) to help users perform tasks. Office productivity suites, data-bs management software, media players and security. "
+    }
+    ,{"heading":"Software Maintenance",
+    "image":applicationimage,
+        "description":"Software Maintenance is the process of modifying a software product after it has been delivered to the customer. The main purpose of software maintenance is to modify and update software applications after delivery to correct faults and to improve performance."
+    }
+    ,{"heading":"OutSourcing",
+    "image":applicationimage,
+        "description":"organization chooses to hire an external software development agency to effectively carry out all the tasks of a software development project, that could be done in-house instead."
+    }
+    ,{"heading":"Training",
+    "image":applicationimage,
+        "description":"We provides more transparency to your training records for everyone and define who can log in, view or edit the training records database. Invite employees to view their own training history."
+    }
+]
+
+
+export default data
