e181f708a45f2bb150b237abe7edbb7cc8384201
Prithivi36 <prithivipappian@gmail.com>
2024-05-11 13:30:40 +0530

    Added All components to page

---
+++ src/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Ahimaz from '../assets/Ahimaz.png'
+
+function About() {
+  return (
+    <>
+    <div className="">
+        <div className="card">
+            <div style={{backgroundImage:`url${Ahimaz}`}} className="">d</div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
   return (
     <>
       <Navbar />
-      
+      <Homepage />
+      <Services />
+      <About />
     </>
   )
 }
